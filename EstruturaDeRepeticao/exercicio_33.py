c35509d2b0b2e2b900c7aecdd9bd1672a5619ade
Gabriel Malachias Hangrad <98166016+AyakuS@users.noreply.github.com>
2022-01-22 08:55:00 -0300

    Update exercicio_33.py

--- EstruturaDeRepeticao/exercicio_33.py
+++ EstruturaDeRepeticao/exercicio_33.py
@@ -3,13 +3,21 @@ O Departamento Estadual de Meteorologia lhe contratou para desenvolver um progra
 de temperaturas, e informe ao final a menor e a maior temperaturas informadas, bem como a média das temperaturas.
 
 """
-lista_temperaturas = [88,28,35,37,38,21,19,5,8,28,35,45,55]
-count = 0
+lista_temperaturas = [28,35,37,38,21,19,5,8,28,35,45,55]
+maior, menor = None, None
+media = 0
 
-while count < len(lista_temperaturas):
-    aux = lista_temperaturas[count+1]
-    if lista_temperaturas[count] > aux:
-        maior = lista_temperaturas[count]
-        
-    count+=1
+for n in range(len(lista_temperaturas)):
+    media += lista_temperaturas[n]
+    if n == 0:
+        maior = menor = lista_temperaturas[n]
+    else:
+        if lista_temperaturas[n] > maior:
+            maior = lista_temperaturas[n]
+        if lista_temperaturas[n] < menor:
+            menor = lista_temperaturas[n]
 
+
+print(f"Maior Temperatura: {maior}°C")
+print(f"Menor temperatura: {menor}°C")
+print(f"Média dos dias: {media/len(lista_temperaturas)}°C")
